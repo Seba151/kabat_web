c316672b4a3856becc1bb3aefa1298396d1dc80f
seba151 <klejchsebastian@gmail.com>
2024-11-12 16:55:28 +0100

    hotove kontakty

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,11 @@ import '../scss/styles.scss'
 // Import all of Bootstrap's JS
 import {Dropdown} from 'bootstrap';
 
+
+  document.addEventListener('DOMContentLoaded', function () {
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+      return new bootstrap.Popover(popoverTriggerEl)
+    })
+  });
+
